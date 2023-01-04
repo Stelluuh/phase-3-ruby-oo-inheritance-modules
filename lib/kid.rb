6ec2d28c9496f5121e9a5392e44a0eacafd3f0fe
stella <sftamesis13@gmail.com>
2023-01-04 15:50:13 -0500

    commit tree 9b842fa267c593789795cbb741a5a852c822f6c4

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Kid
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+    
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
+
+# Remember, include is used to add functionality to our classes via instance methods. The InstanceMethods module inside the FancyDance module contains the methods twirl, jump, pirouette, and take_a_bow, which any instance of the Dancer or Kid class can do.
+
